f016363e35b0d5ec69cf1e2559c3be443310cfb4
perseverance484 <288385517+perseverance484@users.noreply.github.com>
2026-09-03 02:13:23 +0000

    loader: pin builder to 387a59c020e44ff28877483160f7bd3e445fe75f via jsDelivr [auto]

--- builder_loader_user.js
+++ builder_loader_user.js
@@ -7,5 +7,5 @@
 // @match        *://theninja-rpg.com/*
 // @grant        none
 // @run-at       document-idle
-// @require      https://cdn.jsdelivr.net/gh/perseverance484/tnr-tools@75b8bd24701753f001fab89db515f09351bb645e/builder_bundle.js
+// @require      https://cdn.jsdelivr.net/gh/perseverance484/tnr-tools@387a59c020e44ff28877483160f7bd3e445fe75f/builder_bundle.js
 // ==/UserScript==
